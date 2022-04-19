c326fd44f5b9da055bbed8b5a5f259690ade2936
Omega1902 <27062486+Omega1902@users.noreply.github.com>
2022-04-19 21:35:49 +0200

    :white_check_mark: minor change testcase

--- test_corona.py
+++ test_corona.py
@@ -1,18 +1,14 @@
 import unittest
-import asyncio
+
 from corona import Connector
 
 
-async def check_germany():
-    async with Connector() as con:
-        result = await con.get_germany()
-    assert isinstance(result, (float, int))
-    # assert result == 14.5 # enable line and put the real result of the current day for testing
-
-
-class TestCorona(unittest.TestCase):
-    def test_corona_germany(self):
-        asyncio.run(check_germany())
+class TestCorona(unittest.IsolatedAsyncioTestCase):
+    async def test_corona_germany(self):
+        async with Connector() as con:
+            result = await con.get_germany()
+            self.assertIsInstance(result, (float, int))
+            # assert result == 14.5 # enable line and put the real result of the current day for testing
 
 
 if __name__ == "__main__":
