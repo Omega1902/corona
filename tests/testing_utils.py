871d4de3e84f82ede89b08861ff826cc41793c0a
Omega1902 <27062486+Omega1902@users.noreply.github.com>
2023-01-29 18:32:36 +0100

    :green_heart: fixing encoding issue on windows

--- tests/testing_utils.py
+++ tests/testing_utils.py
@@ -6,7 +6,7 @@ def get_testdata_file(filename: str) -> Path:
 
 
 def get_testdata_text(filename: str) -> str:
-    return get_testdata_file(filename).read_text()
+    return get_testdata_file(filename).read_text("utf-8")
 
 
 def get_testdata_binary(filename: str) -> bytes:
