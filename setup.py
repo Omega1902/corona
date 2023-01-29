6605f843b5bf87846ddd7ed995889bbbb43deb09
Omega1902 <27062486+Omega1902@users.noreply.github.com>
2023-01-29 15:36:48 +0100

    :green_heart: refactor tox usage and gh actions

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup
 
+tests_deps = [
+    "flake8==6.0.0",
+    "coverage",
+]
+
+extras = {"test": tests_deps}
+
 if __name__ == "__main__":
-    setup()
+    setup(tests_deps=tests_deps, extras_require=extras)
